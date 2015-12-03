b7a78a2eeb383f8de7fbadad0a6d87ec454cc041
Matthew Horridge <matthew.horridge@stanford.edu>
2015-12-02 17:22:12 -0800

    Update bundle plugin

--- src/main/java/org/coode/dlquery/OWLClassExpressionEditorViewComponent.java
+++ src/main/java/org/coode/dlquery/OWLClassExpressionEditorViewComponent.java
@@ -48,7 +48,7 @@ import static org.coode.dlquery.ResultsSection.*;
  * The University Of Manchester<br>
  * Medical Informatics Group<br>
  * Date: 22-Aug-2006<br><br>
- * <p/>
+ *
  * matthew.horridge@cs.man.ac.uk<br>
  * www.cs.man.ac.uk/~horridgm<br><br>
  */
